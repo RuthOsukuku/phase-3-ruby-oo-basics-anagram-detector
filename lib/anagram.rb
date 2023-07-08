0e3781b3e8c22400a3002f06d368ffb0ff60ad2a
Ruth <osukukuruth@gmail.com>
2023-07-07 22:34:45 -0400

    Done Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w.downcase) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      return false if word == other_word || word.length != other_word.length
+  
+      word.chars.sort == other_word.chars.sort
+    end
+  end
+
+  listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
+# Output: ["inlets"]
